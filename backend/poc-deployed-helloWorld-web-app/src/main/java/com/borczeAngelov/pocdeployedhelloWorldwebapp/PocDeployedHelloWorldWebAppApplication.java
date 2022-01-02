e95403349bf2b82a6c2cc21fe1da40f21c9917e1
Borcze Angelov <Borcze.Angelov@yahoo.com>
2022-01-02 17:12:02 +0100

    commit generated spring boot project

---
+++ backend/poc-deployed-helloWorld-web-app/src/main/java/com/borczeAngelov/pocdeployedhelloWorldwebapp/PocDeployedHelloWorldWebAppApplication.java
@@ -0,0 +1,13 @@
+package com.borczeAngelov.pocdeployedhelloWorldwebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PocDeployedHelloWorldWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PocDeployedHelloWorldWebAppApplication.class, args);
+	}
+
+}
---
+++ backend/poc-deployed-helloWorld-web-app/src/test/java/com/borczeAngelov/pocdeployedhelloWorldwebapp/PocDeployedHelloWorldWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.borczeAngelov.pocdeployedhelloWorldwebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PocDeployedHelloWorldWebAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
